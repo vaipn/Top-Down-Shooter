fc78c4c1548436fe70ee9115e5e30f689cae30a6
Oluwatola Joshua <71330752+vaipn@users.noreply.github.com>
2025-02-15 21:37:53 +0100

    Visual Effect of Bullet Impact

--- Top Down Shooter/Assets/Scripts/Bullet.cs
+++ Top Down Shooter/Assets/Scripts/Bullet.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+	[SerializeField] private GameObject bulletImpactFX;
 	private Rigidbody rb => GetComponent<Rigidbody>();
 	private void OnCollisionEnter(Collision collision)
 	{
+		CreateImpactFX(collision);
 		//rb.constraints = RigidbodyConstraints.FreezeAll;
 		Destroy(gameObject);
+	}
+
+	private void CreateImpactFX(Collision collision)
+	{
+		if (collision.contacts.Length > 0)
+		{
+			ContactPoint contact = collision.contacts[0]; // first point of contact
+
+			GameObject newImpactFX = Instantiate(bulletImpactFX, contact.point, Quaternion.LookRotation(contact.normal));
+
+			Destroy(newImpactFX, 1f);
+		}
 	}
 }
