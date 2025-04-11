b7656d325de80cb8c2954d6b9361cb9e192b15dd
Oluwatola Joshua <71330752+vaipn@users.noreply.github.com>
2025-04-11 13:04:00 +0100

    Generate cover points for any object
    The cover script has to be attached to the object you want to generate cover points for

---
+++ Top Down Shooter/Assets/Scripts/Enemy/Cover System/Cover.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cover : MonoBehaviour
+{
+    [Header("Cover points")]
+    [SerializeField] private GameObject coverPointPrefab;
+    [SerializeField] private List<CoverPoint> coverPoints = new List<CoverPoint>();
+    [SerializeField] private float xOffset = 1;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private float zOffset = 0.75f;
+
+	private void Start()
+	{
+        GenerateCoverPoints();
+	}
+
+	private void GenerateCoverPoints()
+	{
+		Vector3[] localCoverPoints = {
+			new Vector3 (0, yOffset, zOffset), //front
+			new Vector3 (0, yOffset, -zOffset), //Back
+			new Vector3 (xOffset, yOffset, 0), //Right
+			new Vector3 (-xOffset, yOffset, 0) //Left
+		};
+
+		foreach (Vector3 localPoint in localCoverPoints)
+		{
+			Vector3 worldPoint = transform.TransformPoint (localPoint);
+			CoverPoint coverPoint = Instantiate(coverPointPrefab, worldPoint, Quaternion.identity, transform).GetComponent<CoverPoint>();
+
+			coverPoints.Add (coverPoint);
+		}
+	}
+}
