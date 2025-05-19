90227cb69a0a2d445d73b58ada5500f0052e875a
Oluwatola Joshua <71330752+vaipn@users.noreply.github.com>
2025-05-19 14:27:56 +0100

    UI - make some objects transparent on mouse hover

---
+++ Top Down Shooter/Assets/Scripts/UI/UI_TransparentOnHover.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_TransparentOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	private Dictionary<Image, Color> originalImageColors = new Dictionary<Image, Color>();
+	private Dictionary<TextMeshProUGUI, Color> originalTextColors = new Dictionary<TextMeshProUGUI, Color>();
+
+	private void Start()
+	{
+		foreach (var image in GetComponentsInChildren<Image>(true))
+		{
+			originalImageColors[image] = image.color;
+		}
+
+		foreach (var text in GetComponentsInChildren<TextMeshProUGUI>(true))
+		{
+			originalTextColors[text] = text.color;
+		}
+	}
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		// set all images to transparent
+		foreach (var image in originalImageColors.Keys)
+		{
+			var color = image.color;
+			color.a = 0.15f;
+			image.color = color;
+		}
+
+		foreach (var text in originalTextColors.Keys)
+		{
+			var color = text.color;
+			color.a = 0.15f;
+			text.color = color;
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		// restore to original transparency
+		foreach (var image in originalImageColors.Keys)
+		{
+			image.color = originalImageColors[image];
+		}
+
+		foreach (var text in originalTextColors.Keys)
+		{
+			text.color = originalTextColors[text];
+		}
+	}
+}
