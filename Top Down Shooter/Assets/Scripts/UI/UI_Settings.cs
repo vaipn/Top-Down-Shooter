729f9cff36617befbeb156def389dd1422b6aa25
Oluwatola Joshua <71330752+vaipn@users.noreply.github.com>
2025-05-30 18:43:07 +0100

    Audio & UI - Make slider in settings change volume

--- Top Down Shooter/Assets/Scripts/UI/UI_Settings.cs
+++ Top Down Shooter/Assets/Scripts/UI/UI_Settings.cs
@@ -1,25 +1,37 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class UI_Settings : MonoBehaviour
 {
+	[Header("Audio mixer")]
+	[SerializeField] private AudioMixer audioMixer;
+	private float minVolume = -80f;
+	private float maxVolume = 10f;
+
 	[Header("SFX Settings")]
 	[SerializeField] private Slider sfxSlider;
 	[SerializeField] private TextMeshProUGUI sfxSliderText;
+	[SerializeField] private string sfxParameter;
 
 
 	[Header("BGM Settings")]
 	[SerializeField] private Slider bgmSlider;
 	[SerializeField] private TextMeshProUGUI bgmSliderText;
+	[SerializeField] private string bgmParameter;
 	public void SFXSliderValue(float value)
 	{
-		sfxSliderText.text = Mathf.RoundToInt(value) + "%";
+		sfxSliderText.text = Mathf.RoundToInt(value * 100) + "%";
+		float audioMixerVolume = Mathf.Lerp(minVolume, maxVolume, value);
+		audioMixer.SetFloat(sfxParameter, audioMixerVolume);
 	}
 
 	public void BGMSliderValue(float value)
 	{
-		bgmSliderText.text = Mathf.RoundToInt(value) + "%";
+		bgmSliderText.text = Mathf.RoundToInt(value * 100) + "%";
+		float audioMixerVolume = Mathf.Lerp(minVolume, maxVolume, value);
+		audioMixer.SetFloat(bgmParameter, audioMixerVolume);
 	}
 
 	public void OnFriendlyFireToggle()
