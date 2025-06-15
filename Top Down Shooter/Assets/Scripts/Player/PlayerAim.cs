1aec220b1f17a54540c39af4fe900448a52f1bad
Oluwatola Joshua <71330752+vaipn@users.noreply.github.com>
2025-06-15 14:02:09 +0100

    remove lines stopping build

--- Top Down Shooter/Assets/Scripts/Player/PlayerAim.cs
+++ Top Down Shooter/Assets/Scripts/Player/PlayerAim.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.Intrinsics;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class PlayerAim : MonoBehaviour
